d0b1c7fb2547673234cba729269f9f2275e454ed
Aleais <152950841+Aleais@users.noreply.github.com>
2024-01-13 14:41:51 +0000

    Version 0.1.4
    Added some more improvements...

--- m/events/ready/custom-rp.js
+++ m/events/ready/custom-rp.js
@@ -4,7 +4,7 @@ module.exports = (client) => {
   client.user.setActivity("At Moverse . . .", {
     type: ActivityType.Playing,
     assets: {
-      largeImage: "10-20-5-9-13-22-24m",
+      largeImage: "10-20-5-9-13-22-24m.jpg",
       largeText: "Wanna Join?",
     },
   });
